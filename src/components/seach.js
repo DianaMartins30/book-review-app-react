add36846970c4c8b348b2b9db9741314ff1526c7
Diana Martins <diana30martins30@gmail.com>
2022-07-19 20:48:51 +0100

    update

--- src/components/seach.js
+++ src/components/seach.js
@@ -1,5 +1,40 @@
+import { useState } from "react";
+
 const Search = () => {
-    return ('hello');
-}
+  const [inputValue, setInputValue] = useState("");
+
+  const [book, setBook] = useState(null);
+
+  const onChangeHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const bookSearched = () => {
+    fetch(`http://openlibrary.org/search.json?q=${inputValue}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setBook(response);
+        
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div className="m-4">
+      <input
+        type="text"
+        name="book"
+        placeholder="Write the name of your favorite book..."
+        onChange={onChangeHandler}
+        value={inputValue}
+      />{" "}
+      <button onClick={bookSearched}>Search</button>
+
+      {book && <div>{book.q}</div>}
+
+    </div>
+  );
+};
 
 export default Search;
